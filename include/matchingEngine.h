233a6d8c4cb5d6e3a31fa3e48d24ac16aeac1f58
yunhanchen2 <135736371+yunhanchen2@users.noreply.github.com>
2024-07-08 16:56:26 +0800

    Update matchingEngine.h

--- include/matchingEngine.h
+++ include/matchingEngine.h
@@ -14,6 +14,7 @@ using namespace chrono;
 
 class DataPassingToThreads{
 public:
+    CSRGraph * graph;
     int * num_of_neighbor;
     int * order;
     int * passing_node_to_thread_of_each;
@@ -22,7 +23,8 @@ public:
     int size_of_neighbor_of_prenode_pattern;
     int number_of_matching;
     int index_of_snapshot;
-    DataPassingToThreads(int *aPassing_node_to_thread_of_each,int aRound_index,int * aNeighbor_of_prenode_pattern,int aSize_of_neighbor_of_prenode_pattern,int aNumber_of_matching,int aIndex_of_snapshot,int * aNum_of_neighbor,int * aOrder){
+    int * matching_weight_to_thread_of_each;
+    DataPassingToThreads(int *aPassing_node_to_thread_of_each,int aRound_index,int * aNeighbor_of_prenode_pattern,int aSize_of_neighbor_of_prenode_pattern,int aNumber_of_matching,int aIndex_of_snapshot,int * aNum_of_neighbor,int * aOrder,int * aMatching_weight_to_thread_of_each){
         passing_node_to_thread_of_each=aPassing_node_to_thread_of_each;
         round_index=aRound_index;
         neighbor_of_prenode_pattern=aNeighbor_of_prenode_pattern;
@@ -31,6 +33,7 @@ public:
         index_of_snapshot=aIndex_of_snapshot;
         num_of_neighbor=aNum_of_neighbor;
         order=aOrder;
+        matching_weight_to_thread_of_each=aMatching_weight_to_thread_of_each;
     }
 };
 
@@ -38,6 +41,9 @@ public:
 struct DataForPassingBack{
     int number_of_matching_node;
     vector <int> matching_node;
+    vector <int> matching_weight;
+    int _old;
+    int _new;
 };
 
 struct ThreadData{
@@ -49,27 +55,31 @@ struct ThreadData{
 class matchingEngine{
 
 private:
-    vector<int> node_of_matching;  //record all the matching (n*m) m is the number of the node in pattern graph
-    ThreadData *args; //the data collection of the data passing to threads
-    int number_of_thread;  //number of threads to do the matching
-    int number_of_node_for_last_matching; //the number of matching for the last round
-    CSRGraph *graph;  //point to the CSRGraph object created in main
-    PatternGraph * patternGraph;  //point to the PatternGraph object in main 
+    vector<int> node_of_matching;
+    vector <int> weight_of_matching;
+    ThreadData *args;
+    int number_of_thread;
+    int number_of_node_for_last_matching;
+    CSRGraph *graph;
+    PatternGraph * patternGraph;
 
-    //data used in the function of prepare for the data that passed to threads
+    int * _old_counter;
+    int * _new_counter;
+
+    //data used in the function of prepare for the data
     int **passing_node_to_thread_of_each;
+    int **passing_weight_to_thread_of_each;
     DataPassingToThreads **dataPassingToThreads;
     int *number_of_matching;
     int* neighbor_of_prenode;
 
 public:
-    //get the private data
-    vector<int> getNode_of_matching();  
-
-    matchingEngine(int aNumber_of_thread,CSRGraph* aGraph,PatternGraph * aPatternGraph); 
-    ThreadData * get_the_data_prepared(int matching_round,int index_of_snapshot);  //get the data that passing to each threads
-    int receiving_the_data(DataForPassingBack* ptr_get);  //receive the data that passing back from threads
-    void Round_cleaner(int matching_round);  //delete the data
+    matchingEngine(int aNumber_of_thread,CSRGraph* aGraph,PatternGraph * aPatternGraph);
+    ~matchingEngine(){delete [] _old_counter; delete [] _new_counter;}
+    ThreadData * get_the_data_prepared(int matching_round,int index_of_snapshot);
+    int receiving_the_data(DataForPassingBack* ptr_get,int round_index);
+    vector<int> getNode_of_matching();
+    void Round_cleaner(int matching_round);
+    double get_the_duplicate_ratio();
 };
-
 
