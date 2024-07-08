e8037d903578249f0ee6504ebe1d8cf1bbdf7540
yunhanchen2 <135736371+yunhanchen2@users.noreply.github.com>
2024-07-08 16:57:36 +0800

    Update PatternGraph.h

--- include/PatternGraph.h
+++ include/PatternGraph.h
@@ -10,17 +10,16 @@ using namespace std;
 
 class PatternGraph{
 private:
-    int edge; //the number of the current edges
-    int node; //the number of the current nodes
-    int *index_ptr_of_pattern; //the node array of the pattern graph
-    int *indices_of_pattern; //the edge array of the pattern graph
-    int *num_of_neighbor; //the degree of each node
-    int *order; //the matching order
-    vector < vector<int> > neighbor_restriction; //record the index of the neighbor for each node in the pattern graph
+    int edge;
+    int node;
+    int *index_ptr_of_pattern;
+    int *indices_of_pattern;
+    int *num_of_neighbor;
+    int *order;
+    vector < vector<int> > neighbor_restriction;
 
 public:
-    //get the private data
-    int getEdge(); 
+    int getEdge();
     int getNode();
     int * getIndex_ptr_of_pattern();
     int * getIndices_of_pattern();
@@ -30,11 +29,9 @@ public:
 
     PatternGraph(int e,int n);
     ~PatternGraph() { clear(); }
-
-
-    void getTheNeighborOfEachNode(); //get the num_of_neighbor from index_ptr_of_pattern & indices_if_pattern
-    void getTheMatchingOrder();  //get the matching order of pattern graph 
-    void getNeighborRestriction();  //get the index of the neighbor for each node in the pattern graph
-    void clear();  //delete the data
+    void getTheNeighborOfEachNode();
+    void getTheMatchingOrder();
+    void getNeighborRestriction();
+    void clear();
 };
 
